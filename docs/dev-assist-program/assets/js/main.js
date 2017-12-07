83ff32052d6ff72f787ada73918ba484a5245122
siddhantvinchurkar <siddhantvinchurkar@gmail.com>
2017-12-07 21:43:38 +0530

    Made sure ODAP works!

--- docs/dev-assist-program/assets/js/main.js
+++ docs/dev-assist-program/assets/js/main.js
@@ -108,8 +108,8 @@
 		(function() {
 
 			// Vars.
-				var $form = document.querySelectorAll('#signup-form')[0],
-					$submit = document.querySelectorAll('#signup-form input[type="submit"]')[0],
+				var $form = document.querySelectorAll('#submit-form-form')[0],
+					$submit = document.querySelectorAll('#submit-form-form input[type="submit"]')[0],
 					$message;
 
 			// Bail if addEventListener isn't supported.
